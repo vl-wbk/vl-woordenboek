c439969f776fe7298f875b6cc8fdccc2087d27a9
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-02-08 19:05:24 +0100

    Implement database (+ data seeder) voor regios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,5 +11,6 @@ final class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UserTableSeeder::class);
+        $this->call(RegionTableSeeder::class);
     }
 }
