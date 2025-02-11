58703c2fdcaace9b1ac95b9a187e1f8c99cfc92b
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-02-11 11:50:31 +0100

    Fix wording

--- app/Filament/Resources/SuggestionResource/Actions/RejectSuggestionAction.php
+++ app/Filament/Resources/SuggestionResource/Actions/RejectSuggestionAction.php
@@ -20,9 +20,10 @@ final class RejectSuggestionAction extends Action implements ChecksAuthorization
             ->icon('heroicon-o-document-minus')
             ->requiresConfirmation()
             ->modalHeading('Suggestie afwijzen')
-            ->modalDescription('Bij het afwijzen van de suggestie zal u deze niet meer in de lemma kunnen stoppen.')
+            ->modalDescription('Bij het afwijzen van de suggestie zult u die niet meer in het lemma kunnen stoppen.')
             ->visible(fn (Suggestion $suggestion): bool => self::performActionBasedOnStatus($suggestion))
-            ->action(fn (Suggestion $suggestion): bool => $suggestion->update(['status' => SuggestionStatus::Rejected]));
+            ->action(fn (Suggestion $suggestion): bool => $suggestion->update(['status' => SuggestionStatus::Rejected]))
+            ->modalSubmitActionLabel('Afwijzing bevestigen');
     }
 
     public static function performActionBasedOnStatus(Model $model): bool
