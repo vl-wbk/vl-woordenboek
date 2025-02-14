59fb11584e0bb7d501d90790da07e98a0b0fdd0c
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-02-14 04:06:06 +0100

    WIP

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,59 +1,10 @@
 <x-layouts.application-blank title="{{ $user->name }}">
-    {{-- User profile information section --}}
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <div class="d-flex border-bottom pb-3">
-                    <img src="https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png" alt="{{ $user->name }}" class="flex-shrink-0 img-rounded" style="width:60px;height:60px;">
-                    <div class="ms-4 w-100">
-                        <h4 class="text-gold">
-                            {{ $user->name }}
-
-                            @if ($user->is(auth()->user()))
-                                <div class="float-end">
-                                    <a class="btn btn-white shadow-sm" href="">
-                                        <x-heroicon-o-question-mark-circle class="icon me-1"/> Help
-                                    </a>
-
-                                    <a class="btn btn-white shadow-sm" href="">
-                                        <x-heroicon-o-adjustments-horizontal class="icon me-1"/> Instellingen
-                                    </a>
-                                </div>
-                            @endif
-                        </h4>
-
-                        <ul class="inline-list text-muted mb-0 p-0">
-                            <li class="list-inline-item">
-                                <x-heroicon-o-users class="icon me-1"/>
-                                <span class="fw-bold">Gebruikersgroep:</span>  {{ $user->user_type->value }}
-                            </li>
-
-                            <li class="list-inline-item">|</li>
-
-                            <li class="list-inline-item">
-                                <x-heroicon-o-clock class="icon me-1"/>
-                                <span class="fw-bold">Actief sinds:</span>  {{ $user->created_at->format('d/m/Y') }}
-                            </li>
-
-                            <li class="list-inline-item">|</li>
-
-                            <li class="list-inline-item">
-                                <x-heroicon-o-clock class="icon me-1"/>
-                                <span class="fw-bold">Laast gezien:</span> {{ $user->last_seen_at->format('d/m/Y') }}
-                            </li>
-
-                            <li class="list-inline-item">|</li>
-
-                            <li class="list-inline-item">
-                                <x-heroicon-o-pencil-square class="icon me-1"/>
-                                <span class="fw-bold">Bijdrages:</span> 0
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <x-account.profile-information-banner :user=$user/>
             </div>
         </div>
-        {{-- END - user profile information section --}}
 
         <div class="row py-4">
             {{-- Side navigation --}}
@@ -61,7 +12,7 @@
                 <div class="list-group shadow-sm">
                     <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" aria-current="true">
                         Suggesties
-                        <span class="badge bg-green rounded-pill">14</span>
+                        <span class="badge bg-green rounded-pill">0</span>
                     </a>
                 </div>
             </div>
---
+++ resources/views/components/account/profile-information-banner.blade.php
@@ -0,0 +1,49 @@
+<div class="d-flex border-bottom pb-3">
+    <img src="https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png" alt="{{ $user->name }}" class="flex-shrink-0 img-rounded" style="width:60px;height:60px;">
+
+    <div class="ms-4 w-100">
+        <h4 class="text-gold">
+            {{ $user->name }}
+
+            @if ($user->is(auth()->user()))
+                <div class="float-end">
+                    <a class="btn btn-white shadow-sm" href="">
+                        <x-heroicon-o-question-mark-circle class="icon me-1"/> Help
+                    </a>
+
+                    <a class="btn btn-white shadow-sm" href="">
+                        <x-heroicon-o-adjustments-horizontal class="icon me-1"/> Instellingen
+                    </a>
+                </div>
+            @endif
+        </h4>
+
+        <ul class="inline-list text-muted mb-0 p-0">
+            <li class="list-inline-item">
+                <x-heroicon-o-users class="icon me-1"/>
+                <span class="fw-bold">Gebruikersgroep:</span>  {{ $user->user_type->value }}
+            </li>
+
+            <li class="list-inline-item">|</li>
+
+            <li class="list-inline-item">
+                <x-heroicon-o-clock class="icon me-1"/>
+                <span class="fw-bold">Actief sinds:</span>  {{ $user->created_at->format('d/m/Y') }}
+            </li>
+
+            <li class="list-inline-item">|</li>
+
+            <li class="list-inline-item">
+                <x-heroicon-o-clock class="icon me-1"/>
+                <span class="fw-bold">Laast gezien:</span> {{ $user->last_seen_at->format('d/m/Y') }}
+            </li>
+
+            <li class="list-inline-item">|</li>
+
+            <li class="list-inline-item">
+                <x-heroicon-o-pencil-square class="icon me-1"/>
+                <span class="fw-bold">Bijdrages:</span> {{ $contributions }}
+            </li>
+        </ul>
+    </div>
+</div>
