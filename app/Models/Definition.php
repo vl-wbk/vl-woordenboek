39b47c1b21c2654b1a3c6fbd2a80fef2089861dc
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-02-24 14:03:44 +0100

    Enhance Article and Definition models with detailed PHPDoc comments and strict types for improved documentation and clarity. Add labels relationship in Article model and mark definitions method as deprecated.

--- app/Models/Definition.php
+++ app/Models/Definition.php
@@ -8,9 +8,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use JetBrains\PhpStorm\Deprecated;
 use OwenIt\Auditing\Auditable;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 
+#[Deprecated('Data structure wont be used. So we need to remove the definition related code.')]
 final class Definition extends Model implements AuditableContract
 {
     /** @use HasFactory<\Database\Factories\DefinitionFactory> */
