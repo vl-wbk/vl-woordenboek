954b4fb0abf09ebf38dfa6e5b456832fee527838
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-02-24 14:04:34 +0100

    Rename 'value' column to 'name' in labels table for improved clarity and consistency

--- database/migrations/2025_02_23_083121_create_labels_table.php
+++ database/migrations/2025_02_23_083121_create_labels_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('labels', function (Blueprint $table) {
             $table->id();
-            $table->string('value')->unique();
+            $table->string('name')->unique();
             $table->text('description')->nullable();
             $table->timestamps();
         });
