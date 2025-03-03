7fd928197210d7d3abdd4e758f5757ad38f676af
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-03-03 17:41:34 +0100

    Make changes in order to support migrations

--- database/migrations/2025_02_10_131512_create_articles_table.php
+++ database/migrations/2025_02_10_131512_create_articles_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\ArticleStates;
+use App\Enums\LanguageStatus;
 use App\Models\Region;
 use App\Models\User;
 use App\Models\Article;
@@ -19,15 +21,15 @@ return new class extends Migration
             $table->string('index', 1)
                 ->comment('The index column is used in the word index of the application.')
                 ->virtualAs("UPPER(LEFT(word, 1))");
-            $table->smallInteger('state');
+            $table->smallInteger('state')->default(ArticleStates::New->value);
             $table->foreignIdFor(User::class, 'author_id')->nullable()->references('id')->on('users')->nullOnDelete();
             $table->foreignIdFor(User::class, 'editor_id')->nullable()->references('id')->on('users')->nullOnDelete();
             $table->string('word');
-            $table->smallInteger('status');
-            $table->string('description');
+            $table->smallInteger('status')->default(LanguageStatus::Onbekend->value);
+            $table->string('description')->nullable();
             $table->text('keywords')->nullable();
-            $table->text('example');
-            $table->text('characteristics');
+            $table->text('example')->nullable();
+            $table->text('characteristics')->nullable();
             $table->timestamps();
         });
 
