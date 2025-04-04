5aa5ef4691825ed4995dc5c2cd9e5b660976f5e4
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-04-04 03:25:18 +0200

    Terugbrengen van de full text

--- database/migrations/2025_02_10_131512_create_articles_table.php
+++ database/migrations/2025_02_10_131512_create_articles_table.php
@@ -27,10 +27,10 @@ return new class extends Migration
             $table->foreignIdFor(User::class, 'author_id')->nullable()->references('id')->on('users')->nullOnDelete();
             $table->foreignIdFor(User::class, 'editor_id')->nullable()->references('id')->on('users')->nullOnDelete();
             $table->foreignIdFor(User::class, 'archiever_id')->nullable()->constrained();
-            $table->string('word');
+            $table->string('word')->fulltext();
             $table->smallInteger('status')->default(LanguageStatus::Onbekend->value);
             $table->text('description')->nullable();
-            $table->string('keywords')->nullable();
+            $table->string('keywords')->nullable()->fulltext();
             $table->text('example')->nullable();
             $table->text('characteristics')->nullable();
             $table->string('archiving_reason', 350)->nullable();
