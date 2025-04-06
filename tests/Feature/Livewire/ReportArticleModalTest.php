354897de9c2904cda51d276c9149802ef2e48466
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-04-06 04:37:08 +0200

    Bugfix

--- tests/Feature/Livewire/ReportArticleModalTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\ReportArticleModal;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(ReportArticleModal::class)
-        ->assertStatus(200);
-});
