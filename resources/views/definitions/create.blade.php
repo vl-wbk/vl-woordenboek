86c048afa7423d3d05a6ce5197ab21eb712f468a
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-04-16 14:08:21 +0200

    WIP

--- resources/views/definitions/create.blade.php
+++ resources/views/definitions/create.blade.php
@@ -1,112 +1,127 @@
-@extends ('layouts.application', ['title' => 'Nieuwe suggestie'])
+@extends ('layouts.application-blank', ['title' => 'Nieuwe suggestie'])
 
 @section ('content')
-    <div class="card bg-white border-0 shadow-sm">
-        <div class="card-header border-bottom-0">
-            Definitie van een woord insturen
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="color-green float-start">Suggestie indienen</h3>
+
+                <a href="{{ url()->previous() }}" class="btn btn-outline-danger shadow-sm float-end">
+                    <x-heroicon-o-arrow-uturn-left class="icon me-1"/> Annuleren
+                </a>
+            </div>
         </div>
-        <form action="{{ route('definitions.store') }}" id="createSuggestionForm" method="POST" class="card-body">
-            @csrf {{--  Form field protection --}}
+    </div>
 
-            {{-- This hidoden form input connects the currently authenticated user (if there is a user authenticated) --}}
-            <input type="hidden" name="creator" value="{{ optional(auth()->user())->id }}">
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card bg-white border-0 shadow-sm">
+                    <form action="{{ route('definitions.store') }}" id="createSuggestionForm" method="POST" class="card-body">
+                        @csrf {{--  Form field protection --}}
 
-            <p class="card-text mb-0">
-                Elke gebruiker kan nieuwe termen en hun definities aanleveren bij het Vlaams Woordenboek. <br>
-                Met het volgende formulier kunt ge nieuwe beschrijvingen van typisch vlaamse woorden, termen en uitspraken toevoegen aan onze databank.
-            </p>
+                        {{-- This hidoden form input connects the currently authenticated user (if there is a user authenticated) --}}
+                        <input type="hidden" name="creator" value="{{ optional(auth()->user())->id }}">
 
-            <hr class="my-2">
+                        <p class="card-text mb-0">
+                            Elke gebruiker kan nieuwe termen en hun definities aanleveren bij het Vlaams Woordenboek. <br>
+                            Met het volgende formulier kunt ge nieuwe beschrijvingen van typisch vlaamse woorden, termen en uitspraken toevoegen aan onze databank.
+                        </p>
 
-            <div class="form-group">
-                <label for="woord" class="col-form-label">Woord <span class="fw-bold text-danger">*</span></label>
-                <input type="text" name="woord" id="woordHelptext" value="{{ old('woord') }}" class="form-control @error('woord') is-invalid @enderror">
+                        <hr class="my-2">
 
-                @if ($errors->has('woord'))
-                    <x-forms.validation-error field="woord"/>
-                @else
-                    <x-forms.help-text field="woordHelptext" icon="true" text="Het woord, term of de uitspraak die hier wordt gedefinieerd. Gebruik liefst geen hoofdletters."/>
-                @endif
-            </div>
+                        <div class="form-group">
+                            <label for="woord" class="col-form-label">Woord <span class="fw-bold text-danger">*</span></label>
+                            <input type="text" name="woord" id="woordHelptext" value="{{ old('woord') }}" class="form-control @error('woord') is-invalid @enderror">
 
-            <div class="row">
-                <div class="form-group col-4">
-                    <label for="woordsoort" class="col-form-label">Woordsoort</label>
+                            @if ($errors->has('woord'))
+                                <x-forms.validation-error field="woord"/>
+                            @else
+                                <x-forms.help-text field="woordHelptext" icon="true" text="Het woord, term of de uitspraak die hier wordt gedefinieerd. Gebruik liefst geen hoofdletters."/>
+                            @endif
+                        </div>
 
-                    <select name="woordsoort" id="woordsoort" class="form-select">
-                        <option value="">-- selecteer woordsoort --</option>
+                        <div class="row">
+                            <div class="form-group col-4">
+                                <label for="woordsoort" class="col-form-label">Woordsoort</label>
 
-                        @foreach ($partOfSpeeches as $partOfSpeech => $value)
-                            <option value="{{ $partOfSpeech }}" @selected(old('woordsoort') == $partOfSpeech)>
-                                {{ $value }}
-                            </option>
-                        @endforeach
-                    </select>
+                                <select name="woordsoort" id="woordsoort" class="form-select">
+                                    <option value="">-- selecteer woordsoort --</option>
 
-                    <x-forms.help-text field="kenmerkenHelpText" icon="true" text="Wat voor woord is het? Bv. zn, ww, bn."/>
+                                    @foreach ($partOfSpeeches as $partOfSpeech => $value)
+                                        <option value="{{ $partOfSpeech }}" @selected(old('woordsoort') == $partOfSpeech)>
+                                            {{ $value }}
+                                        </option>
+                                    @endforeach
+                                </select>
+
+                                <x-forms.help-text field="kenmerkenHelpText" icon="true" text="Wat voor woord is het? Bv. zn, ww, bn."/>
+                            </div>
+
+                            <div class="form-group col-8">
+                                <label for="kenmerken" class="col-form-label">Kenmerken</label>
+                                <input type="text" name="kenmerken" value=" {{ old('kenmerken') }}" id="kenmerkenHelpText" class="form-control">
+                                <x-forms.help-text field="kenmerkenHelpText" icon="true" text="Lidwoord, geslacht en meervoud – bv. de ~ (v.), ~sen."/>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="beschrijving" class="col-form-label">Beschrijving <span class="text-danger fw-bold">*</span></label>
+                            <textarea name="beschrijving" class="form-control @error('beschrijving') is-invalid @enderror" id="beschrijvingHelpText" cols="4">{{ old('beschrijving') }}</textarea>
+
+                            @if ($errors->has('beschrijving'))
+                                <x-forms.validation-error field="beschrijving"/>
+                            @else
+                                <x-forms.help-text icon="true" field="beschrijvingHelpText" text="Beschrijf de term in Algemeen Beschaafd Vlaams en beperk je tot één betekenis per beschrijving. Voeg andere betekenissen apart toe."/>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="regio" class="col-form-label">Regio <span class="text-danger fw-bold">*</span></label>
+                            <select id="regioHelpText" class="form-control @error('regio') is-invalid @enderror" name="regio[]" multiple size="10">
+                                @foreach ($regions as $region => $value)
+                                    <option value="{{ $region }}" {{ in_array($region, old('regio', [])) ? 'selected' : '' }}>
+                                        {{ $value }}
+                                    </option>
+                                @endforeach
+                            </select>
+
+                            @if ($errors->has('regio'))
+                                <x-forms.validation-error field="regio"/>
+                            @else
+                                <span id="regioHelptext" class="form-text text-muted">
+                                    <x-tabler-info-circle class="icon icon-sm me-1"/> Indien het woord enkel in een lokaal dialect wordt gebruikt, geef dan ook de streek aan waar deze term wordt gebruikt.
+                                    - <a href="{{ route('definitions.region-info') }}" target="_blank">Meer info over de regio's.</a> <br>
+
+                                    U kunt meerdere regio's aanklikken doormiddel van de CRTL toets in te drukken.
+                                </span>
+                            @endif
+                        </div>
+
+                        <hr class="mb-1">
+
+                        <div class="form-group">
+                            <label for="voorbeeld" class="col-form-label">Voorbeeld <span class="fw-bold text-danger">*</span></label>
+                            <textarea name="voorbeeld" id="voorbeeldHelpText" class="form-control @error('voorbeeld') is-invalid @enderror" cols="6">{{ old('voorbeeld') }}</textarea>
+
+                            @if ($errors->has('voorbeeld'))
+                                <x-forms.validation-error field="voorbeeld"/>
+                            @else
+                                <x-forms.help-text icon="true" field="voorbeeldHelpText" text="Geef een voorbeeldzin in Algemeen Beschaafd Vlaams die de hierboven beschreven betekenis van het woord verduidelijkt."/>
+                            @endif
+                        </div>
+                    </form>
+
+                    <div class="card-footer bg-white border-top">
+                        <button type="submit" form="createSuggestionForm" class="btn btn-sm btn-suggestion-submit">
+                            <x-tabler-send class="icon icon-sm me-1" /> Insturen
+                        </button>
+                        <button type="reset" form="createSuggestionForm" class="btn btn-link btn-sm">
+                            <x-tabler-arrow-back-up class="icon icon-sm me-1 text-danger"/> Reset
+                        </button>
+                    </div>
                 </div>
-
-                <div class="form-group col-8">
-                    <label for="kenmerken" class="col-form-label">Kenmerken</label>
-                    <input type="text" name="kenmerken" value=" {{ old('kenmerken') }}" id="kenmerkenHelpText" class="form-control">
-                    <x-forms.help-text field="kenmerkenHelpText" icon="true" text="Lidwoord, geslacht en meervoud – bv. de ~ (v.), ~sen."/>
-                </div>
             </div>
-
-            <div class="form-group">
-                <label for="beschrijving" class="col-form-label">Beschrijving <span class="text-danger fw-bold">*</span></label>
-                <textarea name="beschrijving" class="form-control @error('beschrijving') is-invalid @enderror" id="beschrijvingHelpText" cols="4">{{ old('beschrijving') }}</textarea>
-
-                @if ($errors->has('beschrijving'))
-                    <x-forms.validation-error field="beschrijving"/>
-                @else
-                    <x-forms.help-text icon="true" field="beschrijvingHelpText" text="Beschrijf de term in Algemeen Beschaafd Vlaams en beperk je tot één betekenis per beschrijving. Voeg andere betekenissen apart toe."/>
-                @endif
-            </div>
-
-            <div class="form-group">
-                <label for="regio" class="col-form-label">Regio <span class="text-danger fw-bold">*</span></label>
-                <select id="regioHelpText" class="form-control @error('regio') is-invalid @enderror" name="regio[]" multiple size="10">
-                    @foreach ($regions as $region => $value)
-                        <option value="{{ $region }}" {{ in_array($region, old('regio', [])) ? 'selected' : '' }}>
-                            {{ $value }}
-                        </option>
-                    @endforeach
-                </select>
-
-                @if ($errors->has('regio'))
-                    <x-forms.validation-error field="regio"/>
-                @else
-                    <span id="regioHelptext" class="form-text text-muted">
-                        <x-tabler-info-circle class="icon icon-sm me-1"/> Indien het woord enkel in een lokaal dialect wordt gebruikt, geef dan ook de streek aan waar deze term wordt gebruikt.
-                        - <a href="{{ route('definitions.region-info') }}" target="_blank">Meer info over de regio's.</a> <br>
-
-                        U kunt meerdere regio's aanklikken doormiddel van de CRTL toets in te drukken.
-                    </span>
-                @endif
-            </div>
-
-            <hr class="mb-1">
-
-            <div class="form-group">
-                <label for="voorbeeld" class="col-form-label">Voorbeeld <span class="fw-bold text-danger">*</span></label>
-                <textarea name="voorbeeld" id="voorbeeldHelpText" class="form-control @error('voorbeeld') is-invalid @enderror" cols="6">{{ old('voorbeeld') }}</textarea>
-
-                @if ($errors->has('voorbeeld'))
-                    <x-forms.validation-error field="voorbeeld"/>
-                @else
-                    <x-forms.help-text icon="true" field="voorbeeldHelpText" text="Geef een voorbeeldzin in Algemeen Beschaafd Vlaams die de hierboven beschreven betekenis van het woord verduidelijkt."/>
-                @endif
-            </div>
-        </form>
-
-        <div class="card-footer">
-            <button type="submit" form="createSuggestionForm" class="btn btn-sm btn-suggestion-submit">
-                <x-tabler-send class="icon icon-sm me-1" /> Insturen
-            </button>
-            <button type="rest" form="createSuggestionForm" class="btn btn-reset btn-sm">
-                <x-tabler-arrow-back-up class="icon icon-sm me-1 text-danger"/> Reset
-            </button>
         </div>
     </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,75 +1,161 @@
 
-@extends('layouts.application', ['title' => 'Welkom'])
+@extends('layouts.application-blank', ['title' => 'Welkom'])
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card bg-white border-0 shadow-sm">
-                <div class="card-header border-bottom-0">Zoeken</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="float-start">
+                    <h3 class="color-green">Raadpleging woordenboek</h3>
+                </div>
 
+                <div class="float-end">
+                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with filters and functionalities">
+                        <div class="btn-group me-3 shadow-sm">
+                            <a href="#" class="btn border-0 btn-light active" aria-current="page">
+                                <x-heroicon-o-magnifying-glass-circle class="icon color-green"/> opzoeking
+                            </a>
+                            <a href="#" class="btn border-0 btn-light">
+                                <x-heroicon-o-bookmark class="icon color-green"/> bewaarde woorden
+                            </a>
+                            <a href="#" class="btn border-0 btn-light">
+                                <x-heroicon-o-list-bullet class="icon color-green"/> mijn suggesties
+                            </a>
+                        </div>
+
+                        <div class="btn-group shadow-sm" role="group">
+                            <a href="{{ route('definitions.create') }}" class="btn border-0 btn-submit">
+                                <x-heroicon-s-document-plus class="icon"/> suggestie indienen
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card bg-white border-0 shadow-sm">
+                    <div class="card-header color-green fw-bold bg-white">
+                        Artikel zoeken in het woordenboek
+                    </div>
                     <div class="card-body">
-                        <form id="searchForm" method="GET" action="{{ route('search.results') }}">
-                            <input type="text" class="form-control" name="zoekterm" id="exampleFormControlInput1" value="{{ request()->get('zoekterm') }}" placeholder="Uw zoekterm">
+                        <form action="">
+                            <div class="row g-3">
+                                <div class="col-10">
+                                  <input type="text" class="form-control" placeholder="City" aria-label="City">
+                                </div>
+                                <div class="col-2">
+                                  <button type="submit" class="btn w-100 btn-submit">
+                                    <x-heroicon-o-magnifying-glass class="icon me-1"/> Zoeken
+                                  </button>
+                                </div>
+                              </div>
                         </form>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                    <div class="bg-white card-footer">
-                        <button form="searchForm" class="btn btn-search">Zoeken</button>
-                        <button form="searchForm" class="btn btn-link">reset</button>
+    <div class="container"> {{-- Results & sidebar --}}
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="card bg-callout-card shadow-sm border-0 card-body">
+                    <h5 class="card-title fw-bold fst-italic">Helpende handen gezocht!</h5>
+                    <h6 class="card-subtitle mb-2">Uw inzet, onze kracht</h6>
+
+                    <p class="card-text">
+                        Zin om je handen uit de mouwen te steken en mee te bouwen aan het Vlaams Woordenboek van morgen?
+                        We zijn op zoek naar enthousiaste vrijwilligers die mee het verschil willen maken!
+                    </p>
+
+                    <p class="card-text">
+                        <a href="{{ route('support.volunteers') }}" class="btn btn-white mt-3">
+                            Ik wil meer weten
+                        </a>
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-12">
+
+                            <div class="float-start mb-2">
+                                <span class="fw-bold">23</span> artikelen gevonden
+                            </div>
+
+                            <div class="float-end mb-2">
+                                <ul class="list-inline mb-0">
+                                    <li class="list-inline-item fw-bold text-muted">Sorteer op:</li>
+
+                                    <li class="list-inline-item">
+                                        <a href="">Standaard</a>
+                                    </li>
+
+                                    <li class="list-inline-item text-muted">|</li>
+
+                                    <li class="list-inline-item">
+                                        <a href="">Publicatie</a>
+                                    </li>
+
+                                    <li class="list-inline-item text-muted">|</li>
+
+                                    <li class="list-inline-item">
+                                        <a href="">Weergaves</a>
+                                    </li>
+                                </ul>
+                            </div>
+                    </div>
+                </div>
+
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title color-green">Woord</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">(het; o; meervoud: woorden)</h6>
+                        <p class="card-text">groep van spraakklanken met een eigen betekenis; (bij uitbreiding) het gesprokene: de daad bij het woord voegen doen wat je hebt aangekondigd, beloofd; ...</p>
+
+                        <p class="card-text fw-bold my-2">
+                            Op basis van de suggestie ingestuurd door <span class="color-green">Jan met de pet</span>
+                        </p>
+
+                        <a href="" class="card-link text-decoration-none">
+                            <x-heroicon-o-eye class="icon color-green"/> bekijk
+                        </a>
+
+                        <a href="" class="card-link text-decoration-none">
+                            <x-heroicon-o-bookmark class="icon color-green"/> bewaar
+                        </a>
                     </div>
                 </div>
 
                 <hr>
 
-                @if (request()->has('zoekterm') && $results->total() > 0)
-                    <div class="card bg-white border-0 shadow-sm">
-                        <div class="card-header">Zoekresultaten voor: <strong>'{{ request()->get('zoekterm') }}'</strong></div>
-                        <div class="list-group list-group-flush">
-                            @foreach ($results as $result)
-                                <a href="{{ route('word-information.show', $result) }}" class="list-group-item list-group-item-action" aria-current="true">
-                                    <div class="d-flex w-100 justify-content-between">
-                                        <h5 class="mb-1 color-green">{{ $result->word }}</h5>
-
-                                        <small class="text-muted">
-                                            {{ $result->created_at->diffForHumans() }}
-                                        </small>
-                                    </div>
-
-                                    <p class="mb-2">{{ $result->description }}</p>
-
-                                    @if ($result->author)
-                                        <small class="text-muted">Ingestuurd door: <strong>{{ $result->author->name }}</strong></small>
-                                    @endif
-                                </a>
-                            @endforeach
+                <div class="card border-0 bg-transparent">
+                    <div class="card-body p-0">
+                        <div class="float-start">
+                            toont 1 tot 23 van de 1000 resultaten
                         </div>
+
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination justify-content-end">
+                                <li class="page-item disabled">
+                                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Vorige</a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">Volgende</a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
-
-                    @if ($results->hasPages())
-                        <div class="py-3 mt-3 border-top">
-                            {{ $results->links() }}
-                        </div>
-                    @endif
-                @else
-
-                    <div class="alert alert-info alert-important shadow-sm" role="alert">
-                        <h4 class="alert-heading">Geen woorden gevonden!</h4>
-
-                        <p>
-                            Het lijkt erop dat u geen zoekterm hebt opgegeven en/of er woorden zijn gevonden zijn met uw zoekterm.
-                            Indien het woord dat u zoekt een vlaams woord is en niet is opgenomen in ons woordenboek. Kunt u via de knop hieronder
-                            een suggestie voor het toevoegen van het woord in kwestie aan ons woordenboek.
-                        </p>
-
-                        <hr>
-
-                        <a href="{{ route('definitions.create') }}" class="btn btn-info btn-sm text-white">
-                            Suggestie toevoegen
-                        </a>
-                    </div>
-                @endif
-
+                </div>
             </div>
         </div>
-    </div>
+    </div> {{-- END results & sidebar --}}
 @endsection
