2ab960ca15df394de06ddb81a8eb2bb46971eaa0
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-04-23 12:15:16 +0200

    Implement registration tests

--- tests/Feature/Http/Controllers/Web/Authentication/RegistrationControllerTest.php
+++ tests/Feature/Http/Controllers/Web/Authentication/RegistrationControllerTest.php
@@ -1,4 +1,22 @@
 <?php
 
-test('registration screen can be rendered')->todo();
-test('new users can register')->todo();
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+
+test('registration screen can be rendered', function (): void {
+    get(route('register'))->assertSuccessful();
+});
+
+test('new users can register', function (): void {
+    $response = post(route('register'), data: [
+        'voornaam' => 'Test',
+        'achternaam' => 'User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $response->assertRedirect(route('home'));
+
+    expect(auth()->check())->toBeTrue();
+});
