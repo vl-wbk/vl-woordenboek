4370db63942142a2f967f508a18f4015dc080fa1
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-04-24 02:38:28 +0200

    Add suggestion store test

--- tests/Feature/Http/Controllers/Web/Suggestions/StoreArticleSuggestionTest.php
+++ tests/Feature/Http/Controllers/Web/Suggestions/StoreArticleSuggestionTest.php
@@ -3,9 +3,11 @@
 use App\Models\Article;
 use App\Models\PartOfSpeech;
 use App\Models\Region;
+use App\Models\User;
 use Database\Seeders\PartOfSpeechTableSeeder;
 use Database\Seeders\RegionTableSeeder;
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\post;
 use function Pest\Laravel\seed;
 
@@ -25,13 +27,27 @@ beforeEach(function (): void {
 });
 
 it('can store an article suggestion as guest user', function(): void {
+    $user = User::factory()->create();
+
     post(route('definitions.store'), $this->formData)
         ->assertRedirect()
         ->assertSessionHasNoErrors();
-});
+
+    $this->assertDatabaseMissing(Article::class, ['author_id' => $user->id]);
+})->group('suggestions', 'articles');
 
 it ('can store an article as authenticated user and the auth identifier is attached', function (): void {
-})->skip();
+    $user = User::factory()->create();
 
-it('test all that the needed fields are registered as required', function (): void {
-})->with(['voorbeeld', 'regio', 'woord', 'beschrijving'])->skip();
+    actingAs($user)->post(route('definitions.store'), array_merge($this->formData, ['creator' => $user->id]))
+        ->assertRedirect()
+        ->assertSessionHasNoErrors();
+
+    $this->assertDatabaseHas(Article::class, ['author_id' => $user->id]);
+})->group('suggestions', 'articles');
+
+it('test all that the needed fields are registered as required', function (string $input): void {
+    post(route('definitions.store'), [])->assertSessionHasErrors($input);
+})
+->with(['voorbeeld', 'regio', 'woord', 'beschrijving'])
+->group('suggestions', 'articles');
