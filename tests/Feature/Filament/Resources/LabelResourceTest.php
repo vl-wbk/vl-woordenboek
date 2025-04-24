910e49bc9dbb1d0e381bfb0f91eebd7a31ecb98e
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-04-24 14:12:13 +0200

    Implement tests for the label resource

--- tests/Feature/Filament/Resources/LabelResourceTest.php
+++ tests/Feature/Filament/Resources/LabelResourceTest.php
@@ -1,31 +1,63 @@
 <?php
 
 use App\Filament\Clusters\Articles\Resources\LabelResource\Pages\ListLabels;
+use App\Filament\Clusters\Articles\Resources\LabelResource\Pages\ViewLabel;
 use App\Models\Label;
+use Filament\Tables\Actions\DeleteBulkAction;
 
 use function Pest\Livewire\livewire;
 
+beforeEach(function (): void {
+    actingAsDeveloper();
+});
+
 it ('can render the index page', function(): void {
+    livewire(ListLabels::class)->assertSuccessful();
 })->group('labels');
 
 it ('can render the information page', function (): void {
+    $record = Label::factory()->create();
+
+    livewire(ViewLabel::class, ['record' => $record->getRouteKey()])->assertSuccessful();
 })->group('labels');
 
-it('has columns', function (): void {
-})->group('labels');
+it('has columns', function (string $column): void {
+    livewire(ListLabels::class)->assertTableColumnExists($column);
+})
+->with(['name', 'articles_count', 'description', 'created_at'])
+->group('labels');
 
-it ('can render columns', function (): void {
-})->group('labels');
+it ('can render columns', function (string $column): void {
+    livewire(ListLabels::class)->assertCanRenderTableColumn($column);
+})
+->with(['name', 'articles_count', 'description', 'created_at'])
+->group('labels');
 
-it ('can search columns', function (): void {
-})->group('labels');
+it ('can search columns', function (string $column): void {
+    $records = Label::factory(5)->create();
+    $value = $records->first()->{$column};
 
-it ('can sort columns', function (): void {
-})->group('labels');
+    livewire(ListLabels::class)
+        ->searchTable($value)
+        ->assertCanSeeTableRecords($records->where($column, $value))
+        ->assertCanNotSeeTableRecords($records->where($column, '!=', $value));
+})
+->with(['name'])
+->group('labels');
+
+it ('can sort columns', function (string $column): void {
+    $records = Label::factory(5)->create();
+
+    livewire(ListLabels::class)
+        ->sortTable($column)
+        ->assertCanSeeTableRecords($records->sortBy($column), inOrder: true)
+        ->sortTable($column, 'desc')
+        ->assertCanSeeTableRecords($records->sortByDesc($column), inOrder: true);
+})
+->with(['articles_count', 'name', 'created_at'])
+->group('labels');
 
 it ('can create a record', function (): void {
-    actingAsDeveloper();
-
     livewire(ListLabels::class)
         ->assertActionExists('create')
         ->callAction('create')
@@ -33,19 +65,32 @@ it ('can create a record', function (): void {
 })->group('labels');
 
 it ('can update a record', function (): void {
+    $record = Label::factory()->create();
 
+    livewire(ViewLabel::class, ['record' => $record->getRouteKey()])
+        ->assertActionExists('edit')
+        ->callAction('edit');
 })->group('labels');
 
 it ('can delete a record', function (): void {
+    $record = Label::factory()->create();
+
+    livewire(ViewLabel::class, ['record' => $record->getRouteKey()])
+        ->assertActionExists('delete')
+        ->callAction('delete');
+
+    $this->assertModelMissing($record);
 })->group('labels');
 
 
 it ('can bulk delete records', function (): void {
-})->group('labels');
+    $records = Label::factory(5)->create();
 
-it ('can validate required', function (): void {
-})->group('labels');
+    livewire(ListLabels::class)
+        ->assertTableBulkActionExists('delete')
+        ->callTableBulkAction(DeleteBulkAction::class, $records);
 
-
-it ('can validate unique', function (): void {
+        collect($records)->each(function (Label $label): void {
+            $this->assertModelMissing($label);
+        });
 })->group('labels');
