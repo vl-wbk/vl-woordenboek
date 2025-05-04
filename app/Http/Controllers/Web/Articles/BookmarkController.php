16f402270ddf98b5463db041dce63773e70ded37
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-05 01:27:58 +0200

    ref #184 - implementatie van de manipulaties voor de bookmarks

---
+++ app/Http/Controllers/Web/Articles/BookmarkController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Web\Articles;
+
+use App\Models\Article;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Spatie\RouteAttributes\Attributes\Get;
+use Spatie\RouteAttributes\Attributes\Middleware;
+
+#[Middleware(middleware: ['auth', 'forbid-banned-user'])]
+final readonly class BookmarkController
+{
+    #[Get(uri: 'bookmark/{article}', name: 'bookmark:create')]
+    public function store(Request $request, Article $article): RedirectResponse
+    {
+        $request->user()->bookmarks()->attach($article);
+
+        return redirect()->action(DictionaryArticleController::class, $article);
+    }
+
+    #[Get(uri: 'unbookmark/{article}', name: 'bookmark:remove')]
+    public function delete(Request $request, Article $article): RedirectResponse
+    {
+        $request->user()->bookmarks()->detach($article);
+
+        return redirect()->action(DictionaryArticleController::class, $article);
+    }
+}
