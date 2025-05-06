530392ed441f499187e5cc1af4be6f68fb46c14d
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-07 00:50:38 +0200

    Implement monitor commands

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('ban:delete-expired')
-    ->everyMinute();
+Schedule::command('ban:delete-expired')->everyMinute();
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
+Schedule::command('backup:monitor')->daily()->at('03:00');
