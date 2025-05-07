9a444c978cea47f5b64c575f18e038717640481c
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-07 05:45:39 +0200

    ref #183 - wijzigen van de tekst voor de no results blankslate

--- resources/views/components/definitions/blankslates/new-visit.blade.php
+++ resources/views/components/definitions/blankslates/new-visit.blade.php
@@ -4,8 +4,8 @@
         <h5 class="card-title fw-bold">Zin om te snuisteren in het Vlaams?</h5>
 
         <p class="card-text text-muted">
-            Typ een woord in dat je wil opzoeken, of kies een filter—zoals een streek, categorie of taalvariant.
-            We tonen je dan alle schone woorden die erbij passen. Geen inspiratie? Probeer eens iets alledaags of iets dat je onlangs hebt gehoord!
+            Typ een woord in dat je wil opzoeken. We tonen je dan alle schone woorden die erbij passen. Geen inspiratie? Probeer eens iets alledaags of iets dat je onlangs hebt gehoord!
+            Na het ingeven van een zoekterm. Tonen we je ook de nodige sorteringen zoals Alfabetische volgorde, publicatiedatum en weergaves.
         </p>
     </div>
 </div>
