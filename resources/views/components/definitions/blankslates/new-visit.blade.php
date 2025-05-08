682380cfbabc455d0972235fe4873a4b8e436b09
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-08 06:10:18 +0200

    Fix #183

--- resources/views/components/definitions/blankslates/new-visit.blade.php
+++ resources/views/components/definitions/blankslates/new-visit.blade.php
@@ -4,8 +4,9 @@
         <h5 class="card-title fw-bold">Zin om te snuisteren in het Vlaams?</h5>
 
         <p class="card-text text-muted">
-            Typ een woord in dat je wil opzoeken. We tonen je dan alle schone woorden die erbij passen. Geen inspiratie? Probeer eens iets alledaags of iets dat je onlangs hebt gehoord!
-            Na het ingeven van een zoekterm. Tonen we je ook de nodige sorteringen zoals Alfabetische volgorde, publicatiedatum en weergaves.
+            Typ een woord in dat je wil opzoeken. We tonen je dan alle schone woorden die erbij passen. Geen inspiratie?
+            Probeer eens iets alledaags of iets dat je onlangs hebt gehoord! Nadat je een zoekterm hebt ingegeven, tonen we je ook sorteeropties, zoals alfabetische volgorde,
+            publicatiedatum en aantal weergaves.
         </p>
     </div>
 </div>
