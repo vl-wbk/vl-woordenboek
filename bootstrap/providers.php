4a58a9d89187f36714bdb2890ed13736b5b39e4a
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-08 06:37:51 +0200

    Add missing provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthorizationServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\RateLimitServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\QueueServiceProvider::class,
 ];
