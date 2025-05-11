d180f3ee4f717741518322d4e199bee5ebe9417c
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-11 17:26:18 +0200

    Adjust cron job time

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('ban:delete-expired')->everyMinute();
-Schedule::command('modal:prune')->daily();
+Schedule::command('modal:prune')->daily()->at('02:00');
 Schedule::command('backup:clean')->daily()->at('01:00');
 Schedule::command('backup:run')->daily()->at('01:30');
 Schedule::command('backup:monitor')->daily()->at('03:00');
