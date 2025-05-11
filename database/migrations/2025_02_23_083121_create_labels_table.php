78a83a1258b418e8a91643774849549ce8248c27
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-11 15:36:55 +0200

    Fix: wijzigien van migrations om php artisan migrate:refresh uit te kunnen voeren

--- database/migrations/2025_02_23_083121_create_labels_table.php
+++ database/migrations/2025_02_23_083121_create_labels_table.php
@@ -33,7 +33,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('labels');
         Schema::dropIfExists('article_label');
+        Schema::dropIfExists('labels');
     }
 };
