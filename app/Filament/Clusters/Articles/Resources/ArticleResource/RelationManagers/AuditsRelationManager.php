51f19675ff5b7161cade172362bca1accef8cc56
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-13 08:12:14 +0200

    Update

--- app/Filament/Clusters/Articles/Resources/ArticleResource/RelationManagers/AuditsRelationManager.php
+++ app/Filament/Clusters/Articles/Resources/ArticleResource/RelationManagers/AuditsRelationManager.php
@@ -1,20 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Clusters\Articles\Resources\ArticleResource\RelationManagers;
 
-use Filament\Forms;
-use Filament\Forms\Form;
+use App\Filament\Resources\ArticleResource\Pages\ViewWord;
+use App\UserTypes;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Models\Audit;
 
-class AuditsRelationManager extends RelationManager
+final class AuditsRelationManager extends RelationManager
 {
     protected static string $relationship = 'audits';
+
+    public static function canViewForRecord(Model $ownerRecord, string $pageClass): bool
+    {
+        return $ownerRecord->audits->count() > 0
+            && new $pageClass() instanceof ViewWord
+            && auth()->user()->user_type->isNot(UserTypes::Normal);
+    }
 
     public function table(Table $table): Table
     {
@@ -22,6 +30,9 @@ class AuditsRelationManager extends RelationManager
             ->heading('Artikel wijzigingen')
             ->description('Een overzicht van de recente artikel wijzigingen. De actie om de wijziging te bekijken zal je meenemen naar de voorkant van de applicatie. Dit is wegens dat we meer vrijheid wouden in het weergeven van de informatie.')
             ->recordTitleAttribute('id')
+            ->emptyStateIcon('heroicon-o-eye')
+            ->emptyStateHeading('Er zijn geen handelingen gevonden')
+            ->emptyStateDescription('Het lijkt erop dat er momenteel nog geen handelingen zijn uitgevoerd voor dit artikel. Kom^later nog eens terug.')
             ->columns([
                 Tables\Columns\TextColumn::make('id')
                     ->label('#')
@@ -36,6 +47,7 @@ class AuditsRelationManager extends RelationManager
                     ->label('Uitgevoerd door'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->label('Uitgevoerd op')
+                    ->sortable()
                     ->since(),
             ])
             ->filters([
--- app/Http/Controllers/Web/Articles/ArticleVersionInformationController.php
+++ app/Http/Controllers/Web/Articles/ArticleVersionInformationController.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Web\Articles;
 
+use App\UserTypes;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use OwenIt\Auditing\Models\Audit;
 use Spatie\RouteAttributes\Attributes\Get;
 
 final readonly class ArticleVersionInformationController
 {
     #[Get(uri: '/versie-informatie/{audit}', name: 'change:information', middleware: ['auth', 'forbid-banned-user', 'verified'])]
-    public function __invoke(Audit $audit): Renderable
+    public function __invoke(Request $request Audit $audit): Renderable
     {
+        abort_if($request->user()->user_type->isNot(UserTypes::Normal), Response::HTTP_NOT_FOUND);
+
         return view('versions.info', data: [
             'audit' => $audit
         ]);
