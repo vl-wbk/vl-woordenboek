aad5b9e6b01c953b0dd4d3fcf2f029aeb98cfd9a
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-18 01:49:21 +0200

    Fix: verandering naar een leesbaarder font (Tilt-Neon)

--- resources/views/layouts/application-blank.blade.php
+++ resources/views/layouts/application-blank.blade.php
@@ -12,8 +12,10 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
+
+
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon//favicon-32x32.png') }}">
