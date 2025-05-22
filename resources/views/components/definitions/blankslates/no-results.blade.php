420f6d8bd9d533ff0cbca4991683259276ae8b9e
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-05-23 01:55:35 +0200

    Fix: aanculling bij de melding 'Hela, niks gevonden' (#218)

--- resources/views/components/definitions/blankslates/no-results.blade.php
+++ resources/views/components/definitions/blankslates/no-results.blade.php
@@ -3,10 +3,14 @@
         <x-heroicon-o-book-open class="icon color-green icon-blankslate pb-3"/>
         <h5 class="card-title fw-bold">Spijtig, niks gevonden</h5>
 
-        <p class="card-text text-muted">
+        <p class="card-text text-muted mb-2">
             We vonden geen enkel artikel dat bij jouw zoekterm past.
             Heb je een typfout gemaakt? Misschien is het wel een woord dat of woordcombinatie die (nog) niet in ons woordenboek zit.
             Probeer het eens met een andere zoekterm of een andere schrijfwijze.
+        </p>
+
+        <p class="card-text text-muted">
+            Nog steeds niks? Je mag altijd een suggestie indienen. Dat kan met de knop rechts bovenaan.
         </p>
     </div>
 </div>
