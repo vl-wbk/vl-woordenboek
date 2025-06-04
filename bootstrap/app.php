a0d9cafd688a996b3245118bdb6cf21f0422892b
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-06-04 18:12:54 +0200

    Meh

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Session\Middleware\AuthenticateSession;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,5 +20,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias(['forbid-banned-user' => ForbidBannedUser::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
