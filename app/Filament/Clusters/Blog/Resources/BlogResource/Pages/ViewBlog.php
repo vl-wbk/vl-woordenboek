952277e7f8c98addbc170485c87043622045be21
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-06-08 15:30:01 +0200

    Add ActivateCommentsAction for enabling comments on blog posts; implement action setup with user confirmation and notifications.

--- app/Filament/Clusters/Blog/Resources/BlogResource/Pages/ViewBlog.php
+++ app/Filament/Clusters/Blog/Resources/BlogResource/Pages/ViewBlog.php
@@ -5,30 +5,37 @@ declare(strict_types=1);
 namespace App\Filament\Clusters\Blog\Resources\BlogResource\Pages;
 
 use App\Filament\Clusters\Blog\Resources\BlogResource;
+use App\Filament\Clusters\Blog\Resources\BlogResource\Actions as ResourceSpecificActions;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
-/**
- * @todo Document
- */
 final class ViewBlog extends ViewRecord
 {
-    /**
-     * @todo document
-     */
     protected static string $resource = BlogResource::class;
 
-    /**
-     * @todo document
-     */
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make()
+            Actions\ActionGroup::make([
+                Actions\EditAction::make()
+                    ->color('gray')
+                    ->icon('heroicon-o-pencil-square'),
+
+                ResourceSpecificActions\ActivateCommentsAction::make(),
+                ResourceSpecificActions\DeactivateCommentsAction::make(),
+
+                // Allows deleting the current blog record.
+                // It's wrapped in its own ActionGroup to apply authorization specifically to the delete action.
+                Actions\ActionGroup::make([
+                    Actions\DeleteAction::make()
+                        ->icon('heroicon-o-trash'),
+                ])
+                    ->dropdown(false)
+                    ->authorize('delete', $this->record)
+            ])
+                ->button()
                 ->color('gray')
-                ->icon('heroicon-o-pencil-square'),
-            Actions\DeleteAction::make()
-                ->icon('heroicon-o-trash'),
-        ];
+                ->icon('heroicon-o-cog-8-tooth')
+            ];
     }
 }
