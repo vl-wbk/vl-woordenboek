381849783085b74a7b6ea740ceee53e7241bbc0a
Karel Vervaeke <karel@vervaeke.info>
2025-06-18 21:31:43 +0200

    Database & grants om te zorgen dat testen kunnen lopen

--- mysql-grants.sql
+++ mysql-grants.sql
@@ -4,4 +4,8 @@ CREATE DATABASE vl_woordenboek;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON vl_woordenboek.* TO 'laravel'@'%';
 
+CREATE DATABASE vlaams_woordenboek_testing;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON vlaams_woordenboek_testing.* TO 'laravel'@'%';
+
 FLUSH PRIVILEGES;
