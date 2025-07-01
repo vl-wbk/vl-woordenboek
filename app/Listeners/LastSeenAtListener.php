6a881e1d2b3fac3582c9b87af36439c7294f61f4
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-07-01 09:45:06 +0200

    Quick fix

--- app/Listeners/LastSeenAtListener.php
+++ app/Listeners/LastSeenAtListener.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace App\Listeners;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Login;
 
 final readonly class LastSeenAtListener
 {
     public function handle(Login $loginEvent): void
     {
-        $loginEvent->user->update(['last_seen_at' => now()]);
+        $user = User::query()->findOrFail($loginEvent->id);
+        $user->update(['last_seen_at' => now()]);
     }
 }
