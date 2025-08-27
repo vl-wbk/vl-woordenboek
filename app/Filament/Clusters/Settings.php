dab174e8e14bfe5f709d1c201a9c8463b4ffd63c
Tjoostenm <topairy@gmail.com>
2025-08-28 01:58:21 +0200

    Work on wording and docblocks

--- app/Filament/Clusters/Settings.php
+++ app/Filament/Clusters/Settings.php
@@ -7,9 +7,9 @@ namespace App\Filament\Clusters;
 use Filament\Clusters\Cluster;
 
 /**
- * Represents the settings cluster within the application.
+ * Represents the settings cluster within the app.
  *
- * This class extends the Filament framework's Cluster functionality to manage the settings related to the application.
+ * This class extends the Filament framework's Cluster capability to manage the settings related to the app.
  * It configures aspects such as navigation icons, labels, and breadcrumbs specifically for the settings section in the UI.
  *
  * @package App\Filament\Cluster
@@ -28,17 +28,17 @@ final class Settings extends Cluster
 
     /**
      * The label for this cluster that appears in the navigation menu.
-     * This label 'Instellingen' translates to 'Settings' in English and is displayed to users in the application's navigation menu as a text identifier for accessing the settings section.
+     * This label 'Instellingen' translates to 'Settings' in English and is displayed to users in the app's navigation menu as a text identifier for accessing the settings section.
      *
      * @var ?string A nullable string for the navigation label, allowing for dynamic configuration.
      */
     protected static ?string $navigationLabel = 'Instellingen';
 
     /**
-     * Defines the breadcrumb label for the settings cluster in the application's UI.
+     * Defines the breadcrumb label for the settings cluster in the app's UI.
      *
-     * This breadcrumb 'Applicatie instellingen' translates to 'Application settings' in English, providing a clear and contextually relevant cue for users about their location within the application.
-     * It helps in maintaining a consistent language and navigational element throughout the application, especially useful in multilingual settings.
+     * This breadcrumb 'Applicatie instellingen' translates to 'Application settings' in English, providing a clear and contextually relevant cue for users about their location within the app.
+     * It helps in maintaining a consistent language and navigational element throughout the app, especially useful in multilingual settings.
      *
      * @var ?string A nullable string containing the breadcrumb label in Dutch.
      */
