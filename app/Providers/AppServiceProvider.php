b554fb649a5ea3eea5aee50723eecd0201b9ce8b
Tjoosten <Topairy@gmail.com>
2025-10-02 08:41:09 +0200

    removed: Foutieve shield permissie code in de service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -42,32 +42,6 @@ final class AppServiceProvider extends ServiceProvider
 
         // Prohibit destructieve commands in production environments
         FilamentShield::prohibitDestructiveCommands($this->app->isProduction());
-
-
-        FilamentShield::buildPermissionKeyUsing(
-            function (string $entity, string $affix, string $subject, string $case, string $separator) {
-                return match(true) {
-                    # if `configurePermissionIdentifierUsing()` was used previously, then this needs to be adjusted accordingly
-                    is_subclass_of($entity, Resource::class) => Str::of($affix)
-                        ->snake()
-                        ->append('_')
-                        ->append(
-                            Str::of($entity)
-                                ->afterLast('\\')
-                                ->beforeLast('Resource')
-                                ->replace('\\', '')
-                                ->snake()
-                                ->replace('_', '::')
-                        )
-                        ->toString(),
-                    is_subclass_of($entity, Page::class) => Str::of('page_')
-                        ->append(class_basename($entity))
-                        ->toString(),
-                    is_subclass_of($entity, Widget::class) => Str::of('widget_')
-                        ->append(class_basename($entity))
-                        ->toString()
-                    };
-            });
     }
 
     /**
