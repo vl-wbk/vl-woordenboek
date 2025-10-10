566c5392c33826e86ecfb3555ab06c847b952411
Tjoosten <Topairy@gmail.com>
2025-10-10 08:09:16 +0200

    Fix typo's

--- app/Notifications/InactivityWarningNotification.php
+++ app/Notifications/InactivityWarningNotification.php
@@ -27,8 +27,8 @@ class InactivityWarningNotification extends Notification implements ShouldQueue
         return (new MailMessage)
             ->subject('Je account is gemarkeerd voor verwijdering')
             ->greeting("Hey {$notifiable->name},")
-            ->line('We merken dat het al 5 maanden geleden is dat je bent aangemeld voor het Vlaams Woordenboek')
-            ->line('Om niet onnodig gegevens van gebruikers te bewaren hebben je account gemarkeerd voor verwijderen. En zal verwijderd worden op ' . now()->addMonths(6)->format('d-m-Y'))
-            ->line('Om de toegang tot je account te behouden vragen we je om aan te melden op het Vlaams Woordenboek om de automatische verwijdering te stoppen');
+            ->line('We merken dat het al 5 maanden geleden is dat je bent aangemeld voor het Vlaams Woordenboek.')
+            ->line('Om niet onnodig gegevens van gebruikers te bewaren hebben we je account gemarkeerd voor verwijderen. En zal verwijderd worden op ' . now()->addMonths(6)->format('d-m-Y') . '.')
+            ->line('Om de toegang tot je account te behouden vragen we je om aan te melden op het Vlaams Woordenboek om de automatische verwijdering te stoppen.');
     }
 }
