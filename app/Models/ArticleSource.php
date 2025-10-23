56574284e48e7e13fdea693ffa286abe9fafcd88
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-10-23 02:19:59 +0200

    fix (#374): Serverfout: HTTP 500 weergave artikel (#377)

--- app/Models/ArticleSource.php
+++ app/Models/ArticleSource.php
@@ -11,6 +11,8 @@ class ArticleSource extends Model
 {
     protected $guarded = ['id'];
 
+    protected $with = ['reference'];
+
     public function reference(): BelongsTo
     {
         return $this->belongsTo(ReferenceWork::class, 'reference_work_id');
