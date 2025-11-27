b9343d3cfcf260ef52efa55c4b1a90a60bade89e
Tjoosten <Topairy@gmail.com>
2025-11-27 23:09:52 +0100

    Implement article infolist vertalingen

---
+++ resources/lang/nl/filament/resources/articles.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    'infolist' => [
+        'edit-information-tab' => [
+            'heading' => 'Bewerking informatie',
+            'text-entries' => [
+                'author' => [
+                    'label' => 'Toegevoegd door',
+                    'placeholder' => 'onbekend',
+                ],
+                'created-at' => 'Toegevoegd op',
+                'editor' => [
+                    'placeholder' => '- onbekend / niet toegewezen',
+                    'label' => 'Redactie door',
+                ],
+                'audit-count' => 'Aantal bewerkingen',
+                'updated-at' => 'Laatst gewijzigd',
+                'example' => 'Voorbeeld',
+            ],
+        ],
+        'article-information-tab' => [
+            'heading' => 'Lemma informatie',
+            'text-entries' => [
+                'state' => 'Artikel status',
+                'word' => 'Woord',
+                'part-of-speech' => 'Woordsoort',
+                'characteristics' => 'Kenmerken',
+                'regions' => 'Regio\'s',
+                'description' => 'Beschrijving',
+            ],
+        ],
+        'archive-information-tab' => [
+            'heading' => 'Archiverings informatie',
+            'text-entries' => [
+                'archived-by' => 'Gearchiveerd door',
+            ],
+            'archived-at' => 'Gearchiveerd op',
+            'reason' => [
+                'label' => 'Beweegredenen',
+                'placeholder' => '- geen beweegredenen opgegeven',
+                'table-columns' => [
+                    'name' => 'naslagwerk',
+                    'reference' => 'referentie',
+                    'added-at' => 'Toegevoegd op',
+                ],
+            ],
+        ],
+        'source-information-tab' => [
+            'heading' => 'Bron vermeldingen',
+        ],
+        'publication-information-tab' => [
+            'heading' => 'Publicatie gegevens',
+            'text-entries' => [
+                'disclaimer' => [
+                    'label' => 'Disclaimer',
+                    'placeholder' => '- er is momenteel geen disclaimer actief voor dit artikel',
+                ],
+                'publisher' => 'Gepubliceerd door',
+                'publisher-placeholder' => '- onbekend',
+                'views' => 'Aantal weergaves',
+                'editor' => 'Redactie door',
+                'published-at' => 'Gepubliceerd sinds',
+            ],
+        ],
+    ],
+];
