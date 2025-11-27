830dffe135026da8d8fb0c961c8029677c761562
Tjoosten <Topairy@gmail.com>
2025-11-27 16:22:28 +0100

    Fix(#349): Status van een artikel

---
+++ resources/lang/nl/enums/language-status.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'standaard-nederlands' => 'Standaard nederlands',
+    'standaard-belgisch-nederlands' => 'Standaard Belgisch-Nederlands',
+    'kandidaat-belgisch-nederlands' => 'Kandidaat Belgisch-Nederlands',
+    'geen-standaard-taal' => 'Geen standaardtaal',
+    'onbekend' => 'Onbekend',
+    'descriptions' => [
+        'standaard-nederlands' => 'Een officieel erkend woord dat voorkomt in Nederlandse woordenboeken en algemeen gebruikt wordt in Nederland en Belgie in formele en informele contexten.',
+        'standaard-belgisch-nederlands' => 'Een officieel erkend woord dat specifiek is voor het Belgisch Nederlands, voorkomt in Belgische woordenboeken en algemeen aanvaard is in België.',
+        'kandidaat-belgisch-nederlands' => 'Een woord dat frequent gebruikt wordt in België maar (nog) niet officieel erkend is als standaardtaal, mogelijk op weg naar erkenning.',
+        'onbekend' => 'Een woord waarvan de status nog niet bepaald is of waarover onvoldoende informatie beschikbaar is',
+        'geen-standaard-taal' => 'Een woord dat niet tot de standaardtaal behoort, zoals dialect of streektaal, en niet algemeen aanvaard is in formele contexten.',
+    ],
+];
