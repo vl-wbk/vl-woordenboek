61b82ff959febd472f377686d8b56472e918674a
Tjoosten <Topairy@gmail.com>
2025-11-29 21:41:10 +0100

    Registreer een feature flag voor de two factor authenticatie

--- app/Http/Controllers/Web/Account/TwoFactorAuthenticationController.php
+++ app/Http/Controllers/Web/Account/TwoFactorAuthenticationController.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Web\Account;
 
+use App\Features\TwoFactorAuthentication;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
+use Laravel\Pennant\Feature;
 use Spatie\RouteAttributes\Attributes\Get;
 use Spatie\RouteAttributes\Attributes\Middleware;
 use Spatie\RouteAttributes\Attributes\Prefix;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Prefix(prefix: 'instellingen')]
 #[Middleware(middleware: ['auth', 'forbid-banned-user'])]
@@ -17,6 +20,8 @@ final readonly class TwoFactorAuthenticationController
     #[Get(uri: 'two-factor-authenticatie', name: 'account:settings:two-factor')]
     public function show(Request $request): Renderable
     {
+        abort_if(boolean: Feature::inactive(TwoFactorAuthentication::class), code: Response::HTTP_NOT_FOUND);
+
         return view('account.settings-two-factor-authentication', data: [
             'user' => $request->user(),
         ]);
--- resources/views/account/components/sidebar.blade.php
+++ resources/views/account/components/sidebar.blade.php
@@ -1,13 +1,16 @@
 <div class="list-group mb-sm-3 shadow-sm">
     <a href="{{ route('profile.settings') }}"
-        class="list-group-item border-bottom list-group-item-action {{ active('profile.settings') }} border-0">
+        class="list-group-item border-bottom list-group-item-action {{ active('profile.settings') }}">
         <x-heroicon-o-user-circle class="icon text-gold me-1" /> algemene informatie
     </a>
     <a href="{{ route('profile.settings.security') }}"
-        class="list-group-item list-group-item-action {{ active('profile.settings.security') }} border-0">
+        class="list-group-item list-group-item-action {{ active('profile.settings.security') }}">
         <x-heroicon-o-key class="icon text-gold me-1" /> wachtwoord en authenticatie
     </a>
-    <a href="{{ route('account:settings:two-factor') }}" class="{{ active('account:settings:two-factor') }} list-group-item list-group-item-action border-0">
-        <x-heroicon-o-shield-check class="icon text-gold me-1" /> Two factor authenticatie
-    </a>
+
+    @if (\Laravel\Pennant\Feature::active(\App\Features\TwoFactorAuthentication::class))
+        <a href="{{ route('account:settings:two-factor') }}" class="{{ active('account:settings:two-factor') }} list-group-item list-group-item-action">
+            <x-heroicon-o-shield-check class="icon text-gold me-1" /> Two factor authenticatie
+        </a>
+    @endif
 </div>
