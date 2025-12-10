cd44feccd0d211435a5edaa75dfc44660b8276f2
Tjoosten <Topairy@gmail.com>
2025-12-10 10:58:38 +0100

    chore(#425): Aanpassing van CC-licentie

--- resources/views/layouts/application-blank.blade.php
+++ resources/views/layouts/application-blank.blade.php
@@ -182,7 +182,8 @@
                     </li>
 
                     <li class="mb-2">
-                        {{ __('layout/application.footer.information-section.license-paragraph') }}
+                        De code is gelicensieerd onder de MIT-licentie. De documentatie hiervan is beschikbaar onder de
+                        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" class="text-white">CC BY-NC-SA 4.0 licentie.</a>
                     </li>
 
                     <li class="mb-2 fst-italic brand-gradient">
