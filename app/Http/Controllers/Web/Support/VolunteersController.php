879f3a882657e402f68aa1243102428b122a20af
Tjoostenm <topairy@gmail.com>
2025-12-26 20:07:50 +0100

    WIP

--- app/Http/Controllers/Web/Support/VolunteersController.php
+++ app/Http/Controllers/Web/Support/VolunteersController.php
@@ -6,8 +6,13 @@ namespace App\Http\Controllers\Web\Support;
 
 use App\Settings\VolunteerSettings;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Middleware\Middleware;
 use Spatie\RouteAttributes\Attributes\Get;
 
+/**
+ * @todo Implement docblocks for the controller.
+ */
 final readonly class VolunteersController
 {
     #[Get(uri: 'ondersteuning/vrijwilligers', name: 'support.volunteers')]
@@ -17,4 +22,13 @@ final readonly class VolunteersController
             'pageSettings' => app(VolunteerSettings::class),
         ]);
     }
+	
+	
+	#[Get(uri: 'ondersteuning/vrijwilligers/registreren', name: 'support.volunteers.submit', middleware: ['auth'])]
+	public function apply(?string $role = null): Renderable
+	{
+		return view('info.volunteers.appli cation',[
+		
+		]);
+	}
 }
