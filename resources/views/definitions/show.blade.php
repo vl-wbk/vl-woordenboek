396c7c1285972c38765dd3b7056c7a54bc574260
Tjoostenm <topairy@gmail.com>
2026-01-10 01:40:40 +0100

    Fix type

--- resources/views/definitions/show.blade.php
+++ resources/views/definitions/show.blade.php
@@ -150,7 +150,7 @@
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h5 class="fw-bold mb-0 color-green">Voorbeelden</h5>
                 </div>
-                <div class="mlarkdown-text">
+                <div class="markdown-text">
                     {!! str($word->example)->markdown()->sanitizeHtml() !!}
                 </div>
             </section>
