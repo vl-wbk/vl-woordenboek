e654dcaf57aee89e301c0ab059b393425500c8e0
Tjoostenm <topairy@gmail.com>
2026-01-26 13:27:29 +0100

    **** **

--- app/Filament/Resources/Articles/Schema/ArticleForm.php
+++ app/Filament/Resources/Articles/Schema/ArticleForm.php
@@ -268,10 +268,10 @@ final readonly class ArticleForm
             Select::make('Gerelateerde woorden')
                 ->label('Gerelateerde woorden')
                 ->native(false)
-                ->preload()
+                ->searchable()
                 ->multiple()
                 ->relationship(name: 'related', ignoreRecord: true, titleAttribute: 'word')
-                ->searchable(['word', 'id']),
+                ->getOptionLabelFromRecordUsing(fn (Article $record) => "#{$record->id} - {$record->word}"),
         ];
     }
 
