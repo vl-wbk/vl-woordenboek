9b3a8de594b894201e8760a3ea497e13ebb3989a
Tjoosten <Topairy@gmail.com>
2026-02-10 19:09:32 +0100

    Fix

--- app/Services/ViewCounterService.php
+++ app/Services/ViewCounterService.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Redis;
 
 final readonly class ViewCounterService
 {
-    public function incrementAndSync($model, int $delayInSeconds = 180)
+    public function incrementAndSync($model, int $delayInSeconds = 40)
     {
         $modelName = strtolower(class_basename($model));
         $cacheKey = "{$modelName}:views:{$model->id}";
