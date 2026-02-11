63270949d20ec1aa2f834158d62b46c2d1788c8c
Tjoosten <Topairy@gmail.com>
2026-02-12 00:58:50 +0100

    code changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 /**
@@ -19,6 +20,8 @@ use Illuminate\Database\Seeder;
  */
 final class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Executes the application's database seeding operations.
      *
