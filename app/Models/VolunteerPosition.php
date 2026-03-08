9885e02efc1d777e5a570bcfb2c42522c6df7534
Tjoostenm <topairy@gmail.com>
2026-03-08 22:45:39 +0100

    docs: toevoegen van properties in de VolunteerPosition model

--- app/Models/VolunteerPosition.php
+++ app/Models/VolunteerPosition.php
@@ -9,6 +9,19 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * @property int                                $id                         The unqiezu identifier from the record in the database.  
+ * @property bool                               $is_open                    Boolean flag to indicate that the volunteer position is open for applicants or not.
+ * @property int|null                           $role_id                    The unique identifier from the associated role to the volunteer position. 
+ * @property string                             $associated_user_group      The associated user group that belongs to the volunteer position.
+ * @property string                             $name                       The name of the volunteer position in the Flemisch Dictionary
+ * @property string|null                        $tag_line                   The short tag line that describes the volunteer position in the system. 
+ * @property string                             $description                The full description of the volunteer position in the application.   
+ * @property \Illuminate\Support\Carbon|null    $created_at                 The timestamp that indicates when the volunteer position is createc in the application. 
+ * @property \Illuminate\Support\Carbon|null    $updated_at                 The timestamp that indicates when the volunteer position was last updated in the application.
+ * 
+ * @package App\Models 
+ */
 final class VolunteerPosition extends Model
 {
     use HasRoles;
