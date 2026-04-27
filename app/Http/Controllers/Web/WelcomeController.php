b662aef5f0f9431e5e5b8fdc1b0cece7415c51d6
Tjoostenm <topairy@gmail.com>
2026-04-27 15:24:07 +0200

    fix: Wijzigen van aantal trending words.
    Vroeger namen we 125 woorden in willekeurige volgorde. We hebben dit met de nieuwe snuisterbalk reduceerd tot maximum 15 woorden.
    Wegens dat er geen behoefte meer is voor 125 woorden omdat de balk een statische weergave is geworden.

--- app/Http/Controllers/Web/WelcomeController.php
+++ app/Http/Controllers/Web/WelcomeController.php
@@ -19,7 +19,7 @@ final readonly class WelcomeController
     {
         return view('welcome', data: [
             'searchPatterns' => SearchPatterns::cases(),
-            'trendingWords' => Article::published()->inRandomOrder()->limit(125)->get(),
+            'trendingWords' => Article::published()->inRandomOrder()->limit(15)->get(),
             'wordOfTheDay' => WordOfTheDay::whereDate('scheduled_for', today())->first(),
             'recent' => Article::with('regions')->published()->orderBy('published_at', 'desc')->limit(3)->get(),
             'articleCount' => Article::published()->count('id'),
