db81082682e35bd67d951e299fe30d16511ce5a5
Tjoostenm <topairy@gmail.com>
2026-04-29 19:21:17 +0200

    Fix

--- resources/views/definitions/regions/show.blade.php
+++ resources/views/definitions/regions/show.blade.php
@@ -255,7 +255,7 @@
                                                 </div>
                                                 <div class="col-md-5 mb-3 mb-md-0">
                                                     <p class="text-muted small mb-0 line-clamp-2">
-                                                        {{ $relatedArticle->seoDescription() }}
+                                                        {{ $relatedArticle->seo_description }}
                                                     </p>
                                                 </div>
                                                 <div class="col-md-3 text-md-end">
