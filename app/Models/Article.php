2c1fd58f130db2e91c2d42c72988ecefb2ea0547
Tjoostenm <topairy@gmail.com>
2026-05-12 01:30:36 +0200

    docs: reformat van properties in het article model.
    Alsook is de property van voor de `migration_configuration` column toegevoegd.
    Wegens dat deze ontbrak in de declaratie van de properties.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -52,28 +52,29 @@ use Override;
  * and includes auditing capabilities to track changes. The model supports relationships with authors,
  * editors, regions, and definitions while also providing likability features.
  *
- * @property int $id                 The unique identifier for the article
- * @property string $word               The dictionary word being defined
- * @property ArticleStates $state              The current state of the article in its lifecycle
- * @property string|null $keywords           The keywords that are attached to the article
- * @property string $description        The detailed explanation of the word
- * @property int $author_id          The ID of the user who created the article
- * @property bool $notify_author        The boolean flag to check if the author of the article wants a publication notification.
- * @property LanguageStatus $status             The current language validation status
- * @property DataOrigin $origin             The origin of the data where the dictionary article is based on.
- * @property string|null $example            Optional usage example of the word
- * @property string|null $characteristics    Additional word characteristics
- * @property int|null $editor_id          The ID of the assigned editor
- * @property int|null $part_of_speech_id  The unique ID of the part of speech information.
- * @property string |null $archiving_reason   The reason why the article has been archived.
- * @property ?Carbon $published_at        The timestamp indicating when the article is published. null = unpublished.
- * @property string|null $redirect_article_id The unique identifier from the article that will be redirect to when archiving the parent
- * @property Carbon $archived_at        Timestamp for when the article is archived at
- * @property Carbon $deleted_at         Timestamp for when the article is marked for deletion.
- * @property Carbon $created_at         Timestamp of when the article was created
- * @property Carbon $updated_at         Timestamp of the last update
+ * @property int                   $id                       The unique identifier for the article
+ * @property string                $word                     The dictionary word being defined
+ * @property ArticleStates         $state                    The current state of the article in its lifecycle
+ * @property string|null           $keywords                 The keywords that are attached to the article
+ * @property string                $description              The detailed explanation of the word
+ * @property int                   $author_id                The ID of the user who created the article
+ * @property bool                  $notify_author            The boolean flag to check if the author of the article wants a publication notification.
+ * @property LanguageStatus        $status                   The current language validation status
+ * @property DataOrigin            $origin                   The origin of the data where the dictionary article is based on.
+ * @property string|null           $example                  Optional usage example of the word
+ * @property string|null           $characteristics          Additional word characteristics
+ * @property int|null              $editor_id                The ID of the assigned editor
+ * @property int|null              $part_of_speech_id        The unique ID of the part of speech information.
+ * @property string|null           $archiving_reason         The reason why the article has been archived.
+ * @property ?Carbon               $published_at             The timestamp indicating when the article is published. null = unpublished.
+ * @property string|null           $redirect_article_id      The unique identifier from the article that will be redirect to when archiving the parent
+ * @property Carbon                $archived_at              Timestamp for when the article is archived at
+ * @property Carbon                $deleted_at               Timestamp for when the article is marked for deletion.
+ * @property Carbon                $created_at               Timestamp of when the article was created
+ * @property Carbon                $updated_at               Timestamp of the last update
+ * @property array{examples: bool} $migration_configuration  The json field where we put configuration that indicates when a article is migrated to a new standard
  *
- * @property-read User $author
+ * @property-read User       $author
  * @property-read Disclaimer $disclaimer
  *
  * @method isPublished()
