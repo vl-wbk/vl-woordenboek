cd173e92008423b5f9f5b3759dc6831e8876c4be
Tjoostenm <topairy@gmail.com>
2026-05-30 03:53:56 +0200

    First steps

--- app/Http/Controllers/Web/Articles/ArticleVersionInformationController.php
+++ app/Http/Controllers/Web/Articles/ArticleVersionInformationController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Web\Articles;
 
 use App\UserTypes;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use OwenIt\Auditing\Models\Audit;
 use Spatie\RouteAttributes\Attributes\Get;
@@ -19,7 +20,38 @@ final readonly class ArticleVersionInformationController
         abort_if($request->user()->user_type->is(UserTypes::Normal), Response::HTTP_NOT_FOUND);
 
         return view('versions.info', data: [
+            'recentAudits' => $this->getRecentAudits($audit),
+            'previous' => $this->getPreviousAudit($audit),
+            'next' => $this->getNextAudit($audit),
             'audit' => $audit,
         ]);
+    }
+
+    public function getPreviousAudit(Audit $audit): ?Audit
+    {
+        return Audit::where('auditable_type', $audit->auditable_type)
+            ->where('auditable_id', $audit->auditable_id)
+            ->where('id', '<', $audit->id)
+            ->orderBy('id', 'desc')
+            ->first();
+    }
+
+    public function getNextAudit(Audit $audit): ?Audit
+    {
+        return Audit::where('auditable_type', $audit->auditable_type)
+            ->where('auditable_id', $audit->auditable_id)
+            ->where('id', '>', $audit->id)
+            ->orderBy('id', 'asc')
+            ->first();
+    }
+
+    private function getRecentAudits(Audit $audit): Collection
+    {
+        return Audit::with('auditable')
+            ->where('auditable_type', 'App\Models\Article')
+            ->where('auditable_id', $audit->auditable_id)
+            ->orderBy('created_at', 'desc')
+            ->limit(5)
+            ->get();
     }
 }
