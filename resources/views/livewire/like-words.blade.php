1e2c1710f90f148795d9b27efa568a4c0007baa1
Tjoosten <Topairy@gmail.com>
2026-07-24 14:56:47 +0200

    fix(#618): up en downvotes text onder knoppen

--- resources/views/livewire/like-words.blade.php
+++ resources/views/livewire/like-words.blade.php
@@ -6,27 +6,31 @@
     <div class="d-flex flex-wrap gap-3 align-items-start mb-3">
         {{-- Upvote --}}
         <div class="text-center">
-            <button 
-                wire:click="upvote" 
+            <button
+                wire:click="upvote"
                 class="btn {{ $hasUpvoted ? 'btn-success' : 'btn-outline-success' }} shadow-sm"
             >
                 <x-heroicon-s-hand-thumb-up class="icon me-1"/> Plezant
             </button>
             <div class="text-muted small mt-1">
-                <span id="count-helpful">{{ $upvotesCount }}</span> stemmen
+                <span id="count-helpful">
+                    {{ trans_choice('{1} :count stem|[2,*] :count stemmen', $upvotesCount) }}
+                </span>
             </div>
         </div>
 
         {{-- Downvote --}}
         <div class="text-center">
-            <button 
-                wire:click="downvote" 
+            <button
+                wire:click="downvote"
                 class="btn {{ $hasDownvoted ? 'btn-danger' : 'btn-outline-danger' }} shadow-sm"
             >
                 <x-heroicon-s-hand-thumb-down class="icon me-1"/> Stom
             </button>
             <div class="text-muted small mt-1">
-                <span id="count-nothelpful">{{ $downvotesCount }}</span> stemmen
+                <span id="count-nothelpful">
+                    {{ trans_choice('{1} :count stem|[2,*] :count stemmen', $downvotesCount) }}
+                </span>
             </div>
         </div>
     </div>
