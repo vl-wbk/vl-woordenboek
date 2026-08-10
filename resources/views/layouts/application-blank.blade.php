9a7af2cb7614aa2ad389c9214fc0100fc0a4ace2
Tjoosten <Topairy@gmail.com>
2026-08-10 17:58:28 +0200

    fix: resolve view bug

--- resources/views/layouts/application-blank.blade.php
+++ resources/views/layouts/application-blank.blade.php
@@ -38,151 +38,177 @@
 </head>
 
 <body class="d-flex flex-column h-100">
-    <nav class="navbar navbar-expand-md navbar-dark bg-navbar shadow-sm">
-        <div class="{{ $containerSize ?? 'container-fluid' }}">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('img/app-logo.png') }}" class="rounded me-2" alt="Bootstrap" width="25" height="25"> {{ config('app.name', 'Laravel') }}
-            </a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-navbar shadow-sm">
+    <div class="{{ $containerSize ?? 'container-fluid' }}">
+        <!-- Brand -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('img/app-logo.png') }}" class="rounded me-2" alt="{{ config('app.name', 'Laravel') }}" width="25" height="25">
+            <span class="d-none d-sm-inline">{{ config('app.name', 'Laravel') }}</span>
+        </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <!-- Toggler Button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-                    @auth
-                        @can('access-backend')
-                            <li class="nav-item">
-                                <a href="{{ url('admin') }}" class="nav-link">
-                                    <x-heroicon-s-arrows-right-left class="icon me-1" /> {{ __('layout/application.navigation.user-menu.management-console') }}
-                                </a>
-                            </li>
-                        @endcan
-                    @endauth
-
-                    <li class="nav-item"">
-                        <a href="{{ route('search.results') }}" class="nav-link"">
-                            <x-heroicon-s-magnifying-glass class="icon me-1"/> Zoeken
-                        </a>
-                    </li>
-
-                    <x-random-article-navigation-item/>
-
-                    <li class="nav-item">
-                        <a href="https://www.forum.vlaamswoordenboek.be" class="nav-link" target="_blank">
-                            <x-heroicon-o-chat-bubble-left-right class="icon me-1"/> {{ __('layout/application.footer.community-section.forum') }}
-                        </a>
-                    </li>
-
-                    @if (\App\Models\Blog::count('id') > 0)
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Navigation -->
+            <ul class="navbar-nav me-auto">
+                @auth
+                    @can('access-backend')
                         <li class="nav-item">
-                            <a href="{{ route('news:index') }}" class="nav-link">
-                                <x-heroicon-s-newspaper class="icon"/> {{ __('layout/application.navigation.news') }}
+                            <a href="{{ url('admin') }}" class="nav-link">
+                                <x-heroicon-s-arrows-right-left class="icon me-1" />
+                                <span class="d-none d-lg-inline">{{ __('layout/application.navigation.user-menu.management-console') }}</span>
                             </a>
                         </li>
-                    @endif
+                    @endcan
+                @endauth
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <x-tabler-info-square-rounded class="icon me-1" aria-hidden="true" /> {{ __('layout/application.footer.links-section.project-information') }}
-                        </a>
+                <li class="nav-item">
+                    <a href="{{ route('search.results') }}" class="nav-link">
+                        <x-heroicon-s-magnifying-glass class="icon me-1" />
+                        <span class="d-none d-lg-inline">Zoeken</span>
+                    </a>
+                </li>
 
-                        <ul class="dropdown-menu border-0 shadow-sm">
-                            @if (app(\App\Settings\ProjectInformationSettings::class)->pageActive)
-                                <li>
-                                    <a class="dropdown-item" href="{{ route('project-information') }}">
-                                        <x-tabler-info-square-rounded class="icon text-muted me-1" /> Algemene informatie
-                                    </a>
-                                </li>
-                            @endif
+                <x-random-article-navigation-item />
 
-                            <li>
-                                <a href="{{ route('statistics') }}" class="dropdown-item">
-                                    <x-heroicon-o-presentation-chart-line class="icon text-muted me-1"/> {{ __('pages/statistics.page-title') }}
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+                <li class="nav-item">
+                    <a href="https://www.forum.vlaamswoordenboek.be" class="nav-link" target="_blank" rel="noopener noreferrer">
+                        <x-heroicon-o-chat-bubble-left-right class="icon me-1" />
+                        <span class="d-none d-lg-inline">{{ __('layout/application.footer.community-section.forum') }}</span>
+                    </a>
+                </li>
 
+                @if (\App\Models\Blog::count('id') > 0)
                     <li class="nav-item">
-                        <a href="mailto:contact@vlaamswoordenboek.be" class="nav-link">
-                            <x-heroicon-s-envelope class="icon me-1" /> {{ __('layout/application.footer.links-section.contact') }}
+                        <a href="{{ route('news:index') }}" class="nav-link">
+                            <x-heroicon-s-newspaper class="icon me-1" />
+                            <span class="d-none d-lg-inline">{{ __('layout/application.navigation.news') }}</span>
                         </a>
                     </li>
-                </ul>
+                @endif
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('filament.admin.auth.login'))
-                            <li class="nav-item">
-                                <a class="nav-link {{ active('login') }}" href="{{ route('login') }}">
-                                    <x-tabler-login-2 class="icon me-1" /> {{ __('Login') }}
+                <!-- Project Information Dropdown -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <x-tabler-info-square-rounded class="icon me-1" aria-hidden="true" />
+                        <span class="d-none d-lg-inline">{{ __('layout/application.footer.links-section.project-information') }}</span>
+                    </a>
+
+                    <ul class="dropdown-menu border-0 shadow-sm">
+                        @if (app(\App\Settings\ProjectInformationSettings::class)->pageActive)
+                            <li>
+                                <a class="dropdown-item" href="{{ route('project-information') }}">
+                                    <x-tabler-info-square-rounded class="icon text-muted me-1" />
+                                    <span>Algemene informatie</span>
                                 </a>
                             </li>
                         @endif
-                    @else
-                        <li class="nav-item">
-                            <a href="{{ route('notifications:index') }}" class="nav-link">
-                                @if (auth()->user()->unreadNotifications()->count() > 0)
-                                    <x-heroicon-s-bell-alert class="icon me-1"/>
-                                @else
-                                    <x-heroicon-s-bell class="icon me-1"/>
-                                @endif
 
+                        <li>
+                            <a href="{{ route('statistics') }}" class="dropdown-item">
+                                <x-heroicon-o-presentation-chart-line class="icon text-muted me-1" />
+                                <span>{{ __('pages/statistics.page-title') }}</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a href="mailto:contact@vlaamswoordenboek.be" class="nav-link">
+                        <x-heroicon-s-envelope class="icon me-1" />
+                        <span class="d-none d-lg-inline">{{ __('layout/application.footer.links-section.contact') }}</span>
+                    </a>
+                </li>
+            </ul>
+
+            <!-- Right Side Navigation -->
+            <ul class="navbar-nav ms-auto">
+                @guest
+                    @if (Route::has('filament.admin.auth.login'))
+                        <li class="nav-item">
+                            <a class="nav-link {{ active('login') }}" href="{{ route('login') }}">
+                                <x-tabler-login-2 class="icon me-1" />
+                                <span class="d-none d-lg-inline">{{ __('Login') }}</span>
+                            </a>
+                        </li>
+                    @endif
+                @else
+                    <!-- Notifications -->
+                    <li class="nav-item">
+                        <a href="{{ route('notifications:index') }}" class="nav-link">
+                            @if (auth()->user()->unreadNotifications()->count() > 0)
+                                <x-heroicon-s-bell-alert class="icon" />
+                            @else
+                                <x-heroicon-s-bell class="icon" />
+                            @endif
+                            <span class="">
                                 {{ auth()->user()->unreadNotifications()->where('type', '!=', \Filament\Notifications\DatabaseNotification::class)->count() }}
+                            </span>
+                        </a>
+                    </li>
+
+                    <!-- Messages -->
+                    <li class="nav-item">
+                        <a href="{{ route('profile:inbox') }}" class="nav-link">
+                            <x-heroicon-s-envelope class="icon" />
+                            @if (auth()->user()->unreadMessagesCount() > 0)
+                                <span class="badge bg-danger position-absolute top-0 start-100 translate-middle rounded-pill ps-2">
+                                    {{ auth()->user()->unreadMessagesCount() }}
+                                </span>
+                            @endif
+                        </a>
+                    </li>
+
+                    <!-- User Dropdown -->
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <x-heroicon-s-user-circle class="icon me-1" />
+                            <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end border-0 bg-white shadow-sm">
+                            <a class="dropdown-item" href="{{ route('account:public', auth()->user()) }}">
+                                <x-heroicon-o-user-circle class="text-muted icon me-1" />
+                                {{ __('layout/application.navigation.user-menu.public-profile') }}
                             </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="{{ route('profile:inbox') }}" class="nav-link">
-                                <x-heroicon-s-envelope class="icon me-1"/> {{ auth()->user()->unreadMessagesCount() }}
-                            </a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" role="button" aria-haspopup="true" data-bs-toggle="dropdown" aria-expanded="false">
-                                <x-heroicon-s-user-circle class="icon me-1" /> {{ Auth::user()->name }}
+                            <a class="dropdown-item" href="{{ route('profile.settings') }}">
+                                <x-heroicon-o-cog-8-tooth class="text-muted icon me-1" />
+                                {{ __('layout/application.navigation.user-menu.settings') }}
                             </a>
 
-                            <div class="dropdown-menu border-0 bg-white shadow-sm dropdown-menu-end">
-                                <a class="dropdown-item" href="{{ route('account:public', auth()->user()) }}">
-                                    <x-heroicon-o-user-circle class="text-muted icon me-1"/> {{ __('layout/application.navigation.user-menu.public-profile') }}
-                                </a>
-                                <a class="dropdown-item" href="{{ route('profile.settings') }}">
-                                    <x-heroicon-o-cog-8-tooth class="text-muted icon me-1"/> {{ __('layout/application.navigation.user-menu.settings') }}
-                                </a>
+                            <div class="dropdown-divider"></div>
 
-                                <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('suggestions:index') }}">
+                                <x-heroicon-o-queue-list class="text-muted icon me-1" />
+                                {{ __('layout/application.navigation.user-menu.my-suggestions') }}
+                            </a>
 
-                                <a class="dropdown-item" href="{{ route('suggestions:index') }}">
-                                    <x-heroicon-o-queue-list class="text-muted icon me-1"/> {{ __('layout/application.navigation.user-menu.my-suggestions') }}
-                                </a>
+                            <a class="dropdown-item" href="{{ route('bookmarks:index') }}">
+                                <x-heroicon-o-book-open class="text-muted icon me-1" />
+                                {{ __('layout/application.navigation.user-menu.saved-words') }}
+                            </a>
 
-                                <a class="dropdown-item" href="{{ route('bookmarks:index') }}">
-                                    <x-heroicon-o-book-open class="text-muted icon me-1" aria-hidden="true"/> {{ __('layout/application.navigation.user-menu.saved-words') }}
-                                </a>
+                            <div class="dropdown-divider"></div>
 
-                                <div class="dropdown-divider"></div>
+                            <a href="{{ route('logout') }}" class="dropdown-item link-danger" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <x-heroicon-s-power class="icon text-danger me-1" />
+                                {{ __('layout/application.navigation.user-menu.lgout') }}
+                            </a>
 
-                                <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <x-heroicon-s-power class="icon text-danger me-1" /> {{ __('layout/application.navigation.user-menu.lgout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
     @yield('jumbotron')
 
@@ -191,7 +217,7 @@
     </main>
 
     <footer aria-label="Hoofd footer" class="footer mt-auto pt-4 pt-md-3 mt-5">
-        <div class="container-fluid pt-4 pb-2 py-md-4x text-body-secondary">
+        <div class="container-fluid pt-4 pb-2 py-md-4 text-body-secondary">
             <div class="row">
                 <div class="col-lg-3">
                 <a class="d-inline-flex align-items-center mb-2 text-white text-decoration-none" href="/" aria-label="Bootstrap">
