cd9ec4c82cf2699c39312191ea4514507d4ebc61
Tjoosten <Topairy@gmail.com>
2026-09-07 10:56:17 +0200

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Gebruikersnaam') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('gebruikersnaam') is-invalid @enderror" name="name" value="{{ old('gebruikersnaam') }}" autofocus>
+                                <input id="name" type="text" class="form-control @error('gebruikersnaam') is-invalid @enderror" name="gebruikersnaam" value="{{ old('gebruikersnaam') }}" autofocus>
                                 <x-forms.validation-error field="gebruikersnaam"/>
                             </div>
                         </div>
